5e795d10a82fb38169293fdf4bddbbecee03f1ba
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-08-17 14:26:07 +0200

    Update CI for Dependabot

--- .github/scripts/test-docker-image.sh
+++ .github/scripts/test-docker-image.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 mkdir -p data/tiles
-chmod -R 777 data/
-chown -R 1000 data/
+sudo chmod -R 777 data/
+sudo chown -R 1000 data/
 echo "Available Images:"
 docker images | grep "raylevation"
 echo "Start Container:"
 docker run --name raylevation-docker-test -d --user 1000 -p 8080:8080 -v $(pwd)/data/:/workspace/data/ raynigon/raylevation:latest
 echo "Start connection attempts..."
 attempt=0
-while [ $attempt -le 30 ]; do
+while [ $attempt -le 15 ]; do
   attempt=$((attempt + 1))
   echo "Waiting for server to be up (attempt: $attempt)..."
-  curl -s --retry 30 --retry-connrefused http://localhost:8080/actuator/healthcheck
+  curl -s --retry 3 --retry-connrefused http://localhost:8080/actuator/healthcheck
   result=$?
   if [ $result == 0 ]; then
     echo ""
